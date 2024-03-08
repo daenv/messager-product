ee739f0ac129dcf5d16c69929892ad561e31c229
daenv <daenvle69@gmail.com>
2024-03-08 12:34:37 +0700

    Refactor AuthForm component to include social login buttons and toggle between login and register variants

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,10 +1,24 @@
 'use client';
 import Input from '@/app/components/input/Input';
-import { useState } from 'react';
+import Button from '@/app/components/Button';
+import { useState, useCallback } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { CiLock } from 'react-icons/ci';
+import AuthSocialButton from './AuthSocialButton';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
+
+type Variant = 'LOGIN' | 'REGISTER';
 const AuthForm = () => {
+   const [variant, setVariant] = useState<Variant>('LOGIN');
    const [IsLoading, setIsLoading] = useState(false);
+
+   const toggleVariant = useCallback(() => {
+      if (variant === 'LOGIN') {
+         setVariant('REGISTER');
+      } else {
+         setVariant('LOGIN');
+      }
+   }, [variant]);
 
    const {
       register,
@@ -21,7 +35,7 @@ const AuthForm = () => {
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
          <div className="bg-white px-2 py-8 shadow sm:round-lg sm:px-10">
             <form
-               className="space-y-6"
+               className="space-y-3"
                onSubmit={() => {
                   console.log('OnSubmit form');
                }}
@@ -65,7 +79,47 @@ const AuthForm = () => {
                      placeholder="Enter your password..."
                   />
                </div>
+               <div className="flex items-center justify-center">
+                  <a href="#">Forgot password?</a>
+               </div>
+
+               <div>
+                  <Button disabled={IsLoading} fullWidth type="submit">
+                     {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+                  </Button>
+               </div>
             </form>
+            <div className="mt-6">
+               <div className="relative">
+                  <div className="absolute inset-0 flex items-center">
+                     <div className="w-full border-t border-gray-300" />
+                  </div>
+                  <div className="relative flex justify-center textt-sm">
+                     <span className="bg-while px-2 text-gray-500">Or continue with</span>
+                  </div>
+               </div>
+
+               <div className="mt-6 flex gap-2">
+                  <AuthSocialButton icon={BsGithub} onClick={() => {}} />
+                  <AuthSocialButton icon={BsGoogle} onClick={() => {}} />
+               </div>
+            </div>
+            <div
+               className="
+            flex 
+            gap-2 
+            justify-center 
+            text-sm 
+            mt-6 
+            px-2 
+            text-gray-500
+          "
+            >
+               <div>{variant === 'LOGIN' ? 'New to Talks?' : 'Already have an account?'}</div>
+               <div onClick={toggleVariant} className="underline cursor-pointer">
+                  {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+               </div>
+            </div>
          </div>
       </div>
    );
